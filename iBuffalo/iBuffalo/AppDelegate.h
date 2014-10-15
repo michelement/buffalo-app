f41aa11bfa3d281564412591906d21add1b8d36f
Michele Kim Jenkins <MKJ@Micheles-iMac.local>
2014-10-14 20:14:09 -0700

    Initial project in Objective-C

---
+++ iBuffalo/iBuffalo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iBuffalo
+//
+//  Created by Michele Kim Jenkins on 14.10.14.
+//  Copyright (c) 2014 Buffalo Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iBuffalo/iBuffalo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iBuffalo
+//
+//  Created by Michele Kim Jenkins on 14.10.14.
+//  Copyright (c) 2014 Buffalo Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
